1379b0addb348152c8fd560cc0c81a8980eb2261
James Hankin <james.hankin@bundlestars.com>
2015-03-30 17:25:11 +0100

    Up to page 68. Angular has been modularized and is integrated with node

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,18 @@
 var app = angular.module('app', [])
 
-app.controller('PostsCtrl', function ($scope, $http) {
-	$scope.addPost = function () {
+app.service('PostsSvc', function ($http) {
+    this.fetch = function () {
+        return $http.get('/api/posts')
+    }
+    this.create = function (post) {
+        return $http.post('/api/posts', post)
+    }
+})
+
+app.controller('PostsCtrl', function ($scope, PostsSvc) {
+    $scope.addPost = function () {
         if ($scope.postBody) {
-            $http.post('/api/posts', {
+            PostsSvc.create({
                 username: 'dickeyxxx',
                 body: $scope.postBody
             }).success(function (post) {
@@ -11,9 +20,33 @@ app.controller('PostsCtrl', function ($scope, $http) {
                 $scope.postBody = null
             })
         }
-	}
+    }
 
-    $http.get('/api/posts').success(function (posts) {
+    PostsSvc.fetch().success(function (posts) {
         $scope.posts = posts
     })
 })
+
+
+
+
+
+// var app = angular.module('app', [])
+
+// app.controller('PostsCtrl', function ($scope, $http) {
+// 	$scope.addPost = function () {
+//         if ($scope.postBody) {
+//             $http.post('/api/posts', {
+//                 username: 'dickeyxxx',
+//                 body: $scope.postBody
+//             }).success(function (post) {
+//                 $scope.posts.unshift(post)
+//                 $scope.postBody = null
+//             })
+//         }
+// 	}
+
+//     $http.get('/api/posts').success(function (posts) {
+//         $scope.posts = posts
+//     })
+// })
